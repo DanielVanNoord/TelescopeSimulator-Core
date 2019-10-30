7f409390b11a66170067ce19553597f6ea9440c1
Daniel Van Noord <sg1mash98@gmail.com>
2019-10-29 22:35:40 -0400

    Configured json serializer to not camelcase

--- TelescopeSimulator.Alpaca/Startup.cs
+++ TelescopeSimulator.Alpaca/Startup.cs
@@ -24,7 +24,8 @@ namespace TelescopeSimulator.Alpaca
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(opts => opts.JsonSerializerOptions.PropertyNamingPolicy = null);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
