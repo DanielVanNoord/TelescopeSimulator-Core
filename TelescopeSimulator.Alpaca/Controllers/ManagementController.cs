7e9ef7bed71b6c52c09672bc6f78c7f0bd8fc27b
Daniel Van Noord <sg1mash98@gmail.com>
2019-10-29 21:49:29 -0400

    Added Alpaca Management API Response

--- TelescopeSimulator.Alpaca/Controllers/ManagementController.cs
+++ TelescopeSimulator.Alpaca/Controllers/ManagementController.cs
@@ -1,12 +1,16 @@
 ﻿using ASCOM.Alpaca.Responses;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace TelescopeSimulator.Alpaca.Controllers
 {
     public class ManagementController : Controller
     {
         private static uint transactionID = 0;
+
+        List<AlpacaConfiguredDevice> configuredDevices = new List<AlpacaConfiguredDevice>() { new AlpacaConfiguredDevice(DeviceManager.GetTelescope(0).Name, "Telescope", 0, "EA091417-66DF-4250-8287-FAFAF38A80A6") };
 
         private static uint TransactionID
         {
@@ -18,9 +22,26 @@ namespace TelescopeSimulator.Alpaca.Controllers
 
         [HttpGet]
         [Route("management/apiversions")]
-        public IntListResponse GetApiVersions(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        public IntListResponse ApiVersions(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
         {
             return new IntListResponse(ClientTransactionID, TransactionID, new int[1] { 1 });
+        }
+
+        [HttpGet]
+        [Route("management/v1/description")]
+        public AlpacaDescriptionResponse Description(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.ProductVersion;
+            return new AlpacaDescriptionResponse(ClientTransactionID, TransactionID, new AlpacaDeviceDescription(".Net Core Telescope Simulator", "ASCOM Initiative", version, "Unknown"));
+        }
+
+        [HttpGet]
+        [Route("management/v1/configureddevices")]
+        public AlpacaConfiguredDevicesResponse ConfiguredDevices(int DeviceNumber, int ClientID = -1, uint ClientTransactionID = 0)
+        {
+            return new AlpacaConfiguredDevicesResponse(ClientTransactionID, TransactionID, configuredDevices);
         }
     }
 }
